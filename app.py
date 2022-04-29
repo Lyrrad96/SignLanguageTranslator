8ed22cd7a7bd85d71ca31c6b8119c338bf402d5b
Darryl Andrade <56973672+Lyrrad96@users.noreply.github.com>
2022-04-29 20:31:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import threading
 # loaded_model.load_weights("VGG16-Aug.h5")
 # print("Loaded model from disk")
 
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 # # Category dictionary
 # categories = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
